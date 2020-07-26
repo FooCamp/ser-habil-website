1baedbb6651e06e31d5b50ee1a548a7263d3c80b
Manuel Fernando Barrera <mbarrera@hugeinc.com>
2020-07-26 12:56:14 -0500

    Modified JS.

--- src/js/components/navigation.js
+++ src/js/components/navigation.js
@@ -1,10 +1,12 @@
+import images from '../../static/assets/*.png';
+
 const navigationComponent = (data) => {
 
   const navigation = document.createElement('SECTION');
   const image = import(`../../static/assets/${data.image}`)
   let navigationContent = `
     <header>
-  <img src="${image}" alt="ser habil logo" href="#">
+  <img src="${images['logo']}" alt="ser habil logo" href="#">
   <nav>
     <ul id="myLinks" >
       <li><a href="#inicio"></a>inicio</li>
@@ -16,7 +18,7 @@ const navigationComponent = (data) => {
     </header>
   `
   navigation.innerHTML = navigationContent
-
+  console.log(images)
   return navigation;
 }
 
