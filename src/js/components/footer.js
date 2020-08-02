a8e9a73758b9a49c875c6a0bb2c48ce406b424f0
Valery Rodriguez <valuarterodriguez@gmail.com>
2020-08-02 15:06:45 -0500

    removed the extra lines in the _footer.scss

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -1,20 +1,20 @@
 import { newText, newMultimedia, newLink, newContainer } from './helpers';
 
-const createlinks = (datalinks) =>{
-  const arraylinks = datalinks.map((link)=>{
-    const linksitems = newLink(link.href,link.text,['footer__data-links']);
-    const listlink = newContainer('li',[linksitems],['footer__items-list']);
-    return listlink;
+const createLinks = (dataLinks) =>{
+  const arrayLinks = dataLinks.map((link)=>{
+    const linksItems = newLink(link.href,link.text,['footer__data-links']);
+    const listLink = newContainer('li',[linksItems],['footer__items-list']);
+    return listLink;
   });
-  return arraylinks;
+  return arrayLinks;
 }
 
-const createicons = (dataicons) => {
-  const arrayicons = dataicons.map((icons)=>{
-    const iconsimages = newMultimedia('img',icons.icon.src,icons.icon.alt,['footer__icons']);
-    const iconslinks = newLink(icons.href,'',['footer__icons-links']);
-    iconslinks.target = '_blank';
-    iconslinks.appendChild(iconsimages);
+const createIcons = (dataIcons) => {
+  const arrayIcons = dataIcons.map((icons)=>{
+    const iconsImages = newMultimedia('img',icons.icon.src,icons.icon.alt,['footer__icons']);
+    const iconsLinks = newLink(icons.href,'',['footer__icons-links']);
+    iconsLinks.target = '_blank';
+    iconsLinks.appendChild(iconsImages);
     return iconsLinks
   });
   return arrayicons;
@@ -23,17 +23,16 @@ const createicons = (dataicons) => {
 
 const footerComp = (data) => {
 
-  const logofooter = newMultimedia('img', data.logo.image.src, data.logo.image.alt,['footer__logo']);
-  const linklogo = newLink(data.logo.href,'',['footer__link-logo']);
-  const listlink = createLinks(data.items);
-  const items = newContainer('ul',listlink,['footer__items']);
-  const socialtext = newText('p',data.socialtext,['footer__social-text']);
-  const socialicons = createIcons(data.sociallinks);
-  linklogo.appendChild(logofooter);
-  const footer = newContainer('footer',[linklogo,items,socialtext,...socialicons],['footer']);
+  const logoFooter = newMultimedia('img', data.logo.image.src, data.logo.image.alt,['footer__logo']);
+  const linkLogo = newLink(data.logo.href,'',['footer__link-logo']);
+  const listLink = createLinks(data.items);
+  const items = newContainer('ul',listLink,['footer__items']);
+  const socialText = newText('p',data.socialText,['footer__social-text']);
+  const socialIcons = createIcons(data.socialLinks);
+  linkLogo.appendChild(logoFooter);
+  const footer = newContainer('footer',[linkLogo,items,socialText,...socialIcons],['footer']);
 
   return footer;
-
 };
 
 
