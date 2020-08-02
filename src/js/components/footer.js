8dc3c5e2d3211e9644dda49e5218d0ccefe36298
Valery Rodriguez <valuarterodriguez@gmail.com>
2020-08-02 15:35:52 -0500

    applied and added the space  between each parameter

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -2,8 +2,8 @@ import { newText, newMultimedia, newLink, newContainer } from './helpers';
 
 const createLinks = (dataLinks) =>{
   const arrayLinks = dataLinks.map((link)=>{
-    const linksItems = newLink(link.href,link.text,['footer__data-links']);
-    const listLink = newContainer('li',[linksItems],['footer__items-list']);
+    const linksItems = newLink(link.href, link.text, ['footer__data-links']);
+    const listLink = newContainer('li', [linksItems], ['footer__items-list']);
     return listLink;
   });
   return arrayLinks;
@@ -11,26 +11,26 @@ const createLinks = (dataLinks) =>{
 
 const createIcons = (dataIcons) => {
   const arrayIcons = dataIcons.map((icons)=>{
-    const iconsImages = newMultimedia('img',icons.icon.src,icons.icon.alt,['footer__icons']);
-    const iconsLinks = newLink(icons.href,'',['footer__icons-links']);
+    const iconsImages = newMultimedia('img', icons.icon.src, icons.icon.alt, ['footer__icons']);
+    const iconsLinks = newLink(icons.href, '', ['footer__icons-links']);
     iconsLinks.target = '_blank';
     iconsLinks.appendChild(iconsImages);
     return iconsLinks
   });
-  return arrayicons;
+  return arrayIcons;
 }
 
 
 const footerComp = (data) => {
 
-  const logoFooter = newMultimedia('img', data.logo.image.src, data.logo.image.alt,['footer__logo']);
-  const linkLogo = newLink(data.logo.href,'',['footer__link-logo']);
+  const logoFooter = newMultimedia('img', data.logo.image.src, data.logo.image.alt, ['footer__logo']);
+  const linkLogo = newLink(data.logo.href, '', ['footer__link-logo']);
   const listLink = createLinks(data.items);
-  const items = newContainer('ul',listLink,['footer__items']);
-  const socialText = newText('p',data.socialText,['footer__social-text']);
+  const items = newContainer('ul', listLink, ['footer__items']);
+  const socialText = newText('p', data.socialText, ['footer__social-text']);
   const socialIcons = createIcons(data.socialLinks);
   linkLogo.appendChild(logoFooter);
-  const footer = newContainer('footer',[linkLogo,items,socialText,...socialIcons],['footer']);
+  const footer = newContainer('footer', [linkLogo,items,socialText,...socialIcons], ['footer']);
 
   return footer;
 };
