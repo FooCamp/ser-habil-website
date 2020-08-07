14381daff8291bde0f6bd1a91cb78af1c68f3d27
Manuel Fernando Barrera <mbarrera@hugeinc.com>
2020-08-06 19:30:13 -0500

    Add new component scroll indicator desktop

--- src/js/components/scroll-indicator.js
+++ src/js/components/scroll-indicator.js
@@ -1,7 +1,7 @@
 import { newText, newContainer } from './helpers';
 
 const scrollIndicator = (data) => {
-  const image = newContainer('img', [], ['scroll__image']);
+  const image = newContainer('img', [], ['scroll__image', 'scroll__bounce']);
   const button = newContainer('button', [image], ['scroll__button']);
   image.src = data.image.src;
   image.alt = data.image.alt;
