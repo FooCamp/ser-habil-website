69fe2564e357e9de193ef51c4c8a9f64d0bddfce
carolina <caritonaranjo@gmail.com>
2020-08-09 11:58:28 -0500

    create the styles in sass

--- src/js/components/events-carousel.js
+++ src/js/components/events-carousel.js
@@ -1,12 +1,14 @@
 import { newText, newContainer, newMultimedia } from './helpers'
 
 const createCards = (cardList) => {
-  const cardCenter = cardList.map((card) => {
+  const cardCenter = cardList.map((card, index) => {
     const img = newMultimedia('img', card, ['carousel-card__img']);
     const cardImage = newContainer('a', [img], []);
     cardImage.href = card.url;
     const cardItem = newContainer('li', [cardImage], ['carousel-card']);
-
+    if (!index) {
+      cardItem.classList.add('carousel-card--active');
+    }
     return cardItem;
   });
   return cardCenter;
@@ -19,7 +21,7 @@ const eventsCarousel = (data) => {
   const arrowImageLeft = newMultimedia('img', { src: 'arrow-slider.png', alt: 'arrow carousel' });
   const arrowImageRight = newMultimedia('img', { src: 'arrow-slider.png', alt: 'arrow carousel' });
   const arrowRight = newContainer('button', [arrowImageRight], ['carousel__button', 'carousel__button--right']);
-  const arrowLeft = newContainer('button', [arrowImageLeft], ['carousel_button']);
+  const arrowLeft = newContainer('button', [arrowImageLeft], ['carousel__button']);
   const carousel = newContainer('div', [ arrowLeft, containerCarousel, arrowRight ], ['carousel__container']);
   const subtitle = newText('h3', data.subtitle, ['carousel__title']);
   const text = newText('p', data.text, ['carousel__text']);
