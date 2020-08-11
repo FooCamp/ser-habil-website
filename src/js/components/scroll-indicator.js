f0ab5b8fea0db34f065801d692eb65f0cb4d9f0c
Manuel Fernando Barrera <mbarrera@hugeinc.com>
2020-08-11 14:24:57 -0500

    Add styles to the desktop version

--- src/js/components/scroll-indicator.js
+++ src/js/components/scroll-indicator.js
@@ -2,11 +2,11 @@ import { newText, newContainer } from './helpers';
 
 const scrollIndicator = (data) => {
   const image = newContainer('img', [], ['scroll__image', 'scroll__bounce']);
-  const button = newContainer('button', [image], ['scroll__button']);
+  const text = newText('p', data.text, ['scroll__text']);
+  const button = newContainer('button', [image, text], ['scroll__button']);
   image.src = data.image.src;
   image.alt = data.image.alt;
-  const text = newText('p', data.text, ['scroll__text']);
-  const section = newContainer('section', [button, text], ['scroll']);
+  const section = newContainer('section', [button], ['scroll']);
   const scrollFunction = () => {
     const scrollAnimation = document.getElementById(data.scrollTo);
     const position = scrollAnimation.offsetTop;
