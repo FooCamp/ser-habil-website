ec9859b51c6e8b7a9dd00ce3e3a7bbaae77e842a
Juan Guillermo <juang961225@gmail.com>
2020-09-06 21:10:23 -0500

    Resolve PRs commits

--- src/js/components/image-fundacion.js
+++ src/js/components/image-fundacion.js
@@ -2,7 +2,8 @@ import { newMultimedia, newContainer } from './helpers';
 
 const imageComponet = (data) => {
   const img = newMultimedia('img', data, ['image-fundation__picture']);
-  const secction = newContainer('section', [img], ['image-fundation']);
+  const container = newContainer('div', [img], ['image-fundation__container']);
+  const secction = newContainer('section', [container], ['image-fundation']);
   return secction;
 };
 
