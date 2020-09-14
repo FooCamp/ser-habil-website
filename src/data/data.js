20aea1c2f61964f488fb174eab4deba5474b2ec2
Diego Paez <you@example.comdiegopariza@hotmail.com>
2020-09-14 15:47:35 -0500

    Adding desktop view to contact component

--- src/data/data.js
+++ src/data/data.js
@@ -171,14 +171,6 @@ const pageData = {
         // TODO: complete info contact form
       },
       {
-        component: 'ubication',
-        iframe: {
-          src:
-            'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2432.0906492940917!2d-75.59378303147989!3d6.255443893561203!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x22c97971d770b4ef!2sAquatic%20Complex%20Atanasio%20Girardot!5e0!3m2!1sen!2sus!4v1599412191860!5m2!1sen!2sus',
-          alt: 'ubicacion de la sede principal',
-        },
-      },
-      {
         component: 'contactUs',
         title: 'Ponte en contacto con nosotros',
         text:
@@ -197,6 +189,14 @@ const pageData = {
             texts: ['hola@serhabil.com', 'hola@serhabil.com'],
           },
         ],
+      },
+      {
+        component: 'ubication',
+        iframe: {
+          src:
+            'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2432.0906492940917!2d-75.59378303147989!3d6.255443893561203!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x22c97971d770b4ef!2sAquatic%20Complex%20Atanasio%20Girardot!5e0!3m2!1sen!2sus!4v1599412191860!5m2!1sen!2sus',
+          alt: 'ubicacion de la sede principal',
+        },
       },
     ],
   },
