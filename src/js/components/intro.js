bd01eef8ad6298e723877d0fc99abe6db3fb33d2
carolina <caritonaranjo@gmail.com>
2020-09-21 18:20:15 -0500

    create the "p" for the texts needed

--- src/js/components/intro.js
+++ src/js/components/intro.js
@@ -13,7 +13,13 @@ const nodeImages = (imagesArrays) => {
   });
   return imagesList;
 };
-
+const nodeText = (textArray) => {
+  const textsList = textArray.map((text) => {
+    const textNodes = newText ('p', text , [ 'intro__texts'] )
+    return textNodes;
+  });
+  return textsList;
+};
 /**
  * @param {object} data objecto que contiene la información de componentes.
  * Nos devuelve unos nodos section que contienen un parrafo, un título, dos imagenes principales y dos de fondo.
@@ -22,9 +28,14 @@ const componentIntro = (data) => {
   const paragraph = newText('p', data.text, ['intro__paragraph']);
   const title = newText('h2', data.title, ['intro__title']);
   const imageIntro = nodeImages(data.images);
+
+  let textNodes = [];
+  if (data.texts) textNodes = nodeText(data.texts);
+  const textIntro = newContainer( 'div', [...textNodes], ['intro__texts-conteiner'])
+
   // eslint-disable-next-line prettier/prettier
-  const containerGeneral = newContainer('div', [...imageIntro, title, paragraph], ['intro__container-general']);
-  const section = newContainer('section', [containerGeneral], ['intro']);
+  const containerGeneral = newContainer('div', [...imageIntro, title, paragraph, textIntro], ['intro__container-general']);
+  const section = newContainer('section', [containerGeneral ], ['intro']);
 
   return section;
 };
